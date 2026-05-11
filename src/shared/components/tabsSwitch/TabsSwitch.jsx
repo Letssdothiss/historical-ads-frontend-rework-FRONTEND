c51c89467e393c4bb9f416ccf3aaef4daaf41298
Emil Jonasson Östvall <ej224jt@student.lnu.se>
2026-05-11 23:58:51 +0200

    Fix active color on tabsswitch component.

--- src/shared/components/tabsSwitch/TabsSwitch.jsx
+++ src/shared/components/tabsSwitch/TabsSwitch.jsx
@@ -1,9 +1,11 @@
 import './TabsSwitch.css';
-import React from 'react';
-import { DigiTablist, DigiButton, DigiIconCopy } from '@designsystem-se/af-react';
+import React, { useState } from 'react';
+import { DigiButton, DigiIconCopy } from '@designsystem-se/af-react';
 import { ButtonSize, ButtonVariation } from '@designsystem-se/af';
 
 function TabsSwitch() {
+  const [activeTab, setActiveTab] = useState("platsannonser");
+
   return (
     <div className="tab-menu-container">
       <div className="reset-button-container">
@@ -19,26 +21,47 @@ function TabsSwitch() {
       </div>
       <div className="tabs-switch-container">
         <DigiButton
+          className="tabs-switch__tab-host"
           afId="button-platsannonser"
 	        afSize={ButtonSize.MEDIUM}
-	        afVariation={ButtonVariation.SECONDARY}
-	        afFullWidth={false}>
+	        afVariation={
+            activeTab === 'platsannonser'
+              ? ButtonVariation.PRIMARY
+              : ButtonVariation.SECONDARY
+          }
+	        afFullWidth={false}
+          onAfOnClick={() => setActiveTab('platsannonser')}
+          >
 	        Platsannonser
         </DigiButton>
 
         <DigiButton
+          className="tabs-switch__tab-host"
           afId="button-statistik"
 	        afSize={ButtonSize.MEDIUM}
-	        afVariation={ButtonVariation.SECONDARY}
-	        afFullWidth={false}>
+	        afVariation={
+            activeTab === 'statistik'
+              ? ButtonVariation.PRIMARY
+              : ButtonVariation.SECONDARY
+          }
+	        afFullWidth={false}
+          onAfOnClick={() => setActiveTab('statistik')}
+          >
 	        Statistik
         </DigiButton>
 
         <DigiButton
+          className="tabs-switch__tab-host"
           afId="button-om-datan"
 	        afSize={ButtonSize.MEDIUM}
-	        afVariation={ButtonVariation.SECONDARY}
-	        afFullWidth={false}>
+	        afVariation={
+            activeTab === 'om-datan'
+              ? ButtonVariation.PRIMARY
+              : ButtonVariation.SECONDARY
+          }
+	        afFullWidth={false}
+          onAfOnClick={() => setActiveTab("om-datan")}
+          >
 	        Om datan
         </DigiButton>
       </div>
