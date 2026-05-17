91753607cbf517dd12839e72be0c766b883382a4
Rouni <ra223aa@student.lnu.se>
2026-05-17 14:41:33 +0200

    fix CompetencySearch component for improved readability and structure

--- src/shared/components/competencySearch/CompetencySearch.jsx
+++ src/shared/components/competencySearch/CompetencySearch.jsx
@@ -1,47 +1,42 @@
-import './CompetencySearch.css';
+import './CompetencySearch.css'
 
-import {
-  DigiFormInput,
-  DigiIconInfoCircleSolid,
-} from '@designsystem-se/af-react';
+import { DigiFormInput } from '@designsystem-se/af-react'
 
 import {
   FormInputType,
   FormInputVariation,
   FormInputValidation,
-} from '@designsystem-se/af';
+} from '@designsystem-se/af'
 
-function CompetencySearch({
-  value = '',
-  onChange,
-}) {
+import InfoTooltip from '../infoTooltip/InfoTooltip'
+
+function CompetencySearch({ value = '', onChange }) {
   return (
     <div className="competency-search">
       <div className="competency-search-label-row">
-        <p className="competency-search-label">
-          Sök på kompetenser
-        </p>
+        <p className="competency-search-label">Sök på kompetenser</p>
 
-        <DigiIconInfoCircleSolid />
+        <InfoTooltip label="Information om kompetenssökning">
+          Sökfältet gör det möjligt att söka efter kompetenser. På grund av
+          begränsningar av databasen kan den vara ofullständig. Kompetenser
+          hämtas från fälten must-have, nice-to-have samt berikade variabler.
+        </InfoTooltip>
       </div>
 
       <div className="competency-search-input-wrapper">
         <DigiFormInput
           afId="competency-search-input"
+          afLabel="Sök på kompetenser"
           afVariation={FormInputVariation.MEDIUM}
           afType={FormInputType.TEXT}
           afValidation={FormInputValidation.NEUTRAL}
           afPlaceholder="Sök kompetens"
           afValue={value}
-          onAfOnInput={(event) =>
-            onChange?.(
-              event.detail.target.value
-            )
-          }
+          onAfOnInput={(event) => onChange?.(event.detail.target.value)}
         />
       </div>
     </div>
-  );
+  )
 }
 
-export default CompetencySearch;
+export default CompetencySearch
